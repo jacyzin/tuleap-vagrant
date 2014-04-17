fb6459ba1b57867e3a27635344b0c739a0d6fffd
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-04-17 16:44:29 +0200

    RPM deployment

--- solo/solo.rb
+++ solo/solo.rb
@@ -1,3 +1,3 @@
 file_cache_path "/var/run/chef-solo"
-cookbook_path "/root/chef-solo/cookbooks"
+cookbook_path File.join(File.expand_path(File.dirname(__FILE__)), '../cookbooks');
 #json_attribs "/root/node.json"
